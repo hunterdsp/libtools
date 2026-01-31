6ce255690a7939b871970b37fb7a014889596900
Matthew T. Hunter, Ph.D. <hunter.dsp@gmail.com>
2026-01-30 19:51:49 -0500

    Update install script.

---
+++ buildme.py
@@ -0,0 +1,15 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = []
+# ///
+
+import sys
+import subprocess
+from pathlib import Path
+
+def main():
+    script = Path(__file__).parent.joinpath('buildme')
+    subprocess.call([f"{str(script)}", *sys.argv[1:]])
+
+if __name__ == "__main__":
+    main()
